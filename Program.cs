67e2686735d703ab16fcb41a3b2af4427acaa38f
Arike115 <ajibolaoluwafunke669@gmail.com>
2022-08-29 19:55:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+public class Program
+{
+    public static void main()
+    {
+        Console.WriteLine("Hello my people");
+    }
+    public static void Main()
+    {
+        main();
+        Console.WriteLine("Hello world");
+    }
+
+
+
+}
